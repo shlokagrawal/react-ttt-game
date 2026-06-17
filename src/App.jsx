5f8c372fe14260cbfc9160eca36e18b349660697
Shlok Agrawal <shlokagrawal188@gmail.com>
2026-06-17 14:46:21 -0400

    created player component and its functionality

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Player from "./components/Player";
+
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player1" symbol="X"/>
+          <Player initialName="Player2" symbol="O"/>
+        </ol>
+        GAME BOARD
+      </div>
+      LOGS
+    </main>
+  )
+}
+
+export default App
