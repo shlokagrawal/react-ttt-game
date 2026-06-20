404c2f76838a2e1cf0b39c808114ca894ef11d56
Shlok Agrawal <shlokagrawal188@gmail.com>
2026-06-20 18:35:52 -0400

    applying concept - lifting the state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
+import {useState} from 'react';
+
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 
 function App() {
+
+  const [activePlayer, setActivePlater] = useState('X');
+
+  function handleSelectSquare(){
+    setActivePlater((curActivePlayer) => curActivePlayer ==='X'?'O':'X');
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player1" symbol="X"/>
-          <Player initialName="Player2" symbol="O"/>
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName="Player2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
         GAME BOARD
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
       LOGS
     </main>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,7 +6,7 @@ const initialGameBoard = [
     [null, null, null]
 ];
 
-export default function GameBoard(){
+export default function GameBoard({onSelectSquare, activePlayerSymbol}){
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     function handleSelectSquare(rowIndex, colIndex) {
@@ -15,9 +15,11 @@ export default function GameBoard(){
             // return preGameBoard; // this type of state change can lead to strange bugs or side effects if you have multiple places in your appln that are scheduling same updates for the same state. notes in our copy
             // below is correct way to do 
             const updatedBoard = [...preGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = 'X';
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updatedBoard;
         });
+
+        onSelectSquare();
     }
     return (
         <ol id="game-board">
